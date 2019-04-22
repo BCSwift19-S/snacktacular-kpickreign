ceae5cc43522eae8ca6996e67055bb9c0c87fe0a
kpickreign <pickreig@bc.edu>
2019-04-22 11:48:29 -0400

    9.16 photos

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -24,6 +24,7 @@ class SpotDetailViewController: UIViewController {
     
     var spot: Spot!
     var reviews: Reviews!
+    var photos: Photos!
     var regionDistance: CLLocationDistance = 750 // half mile, meters
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -41,6 +42,8 @@ class SpotDetailViewController: UIViewController {
        // mapView.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         if spot == nil {
             spot = Spot()
@@ -57,6 +60,7 @@ class SpotDetailViewController: UIViewController {
             navigationController?.setToolbarHidden(true, animated: true)
         }
         reviews = Reviews()
+        photos = Photos()
         
         let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
@@ -100,18 +104,6 @@ class SpotDetailViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
-    @IBAction func textFieldReturnPressed(_ sender: UITextField) {
-        sender.resignFirstResponder()
-        spot.name = nameField.text!
-        spot.address = addressField.text!
-        updateUserInterface()
-        
-    }
-    
-    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
-        saveBarButton.isEnabled = !(nameField.text == "")
-    }
-    
     func updateUserInterface() {
         nameField.text = spot.name
         addressField.text = spot.address
@@ -133,8 +125,36 @@ class SpotDetailViewController: UIViewController {
             navigationController?.popViewController(animated: true)
         }
     }
+    
+    func cameraOrLibraryAlert() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: nil)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+        
+    }
+
+    
+    @IBAction func textFieldReturnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        spot.name = nameField.text!
+        spot.address = addressField.text!
+        updateUserInterface()
+        
+    }
+    
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        saveBarButton.isEnabled = !(nameField.text == "")
+    }
+    
+    
 
     @IBAction func photoButtonPressed(_ sender: UIButton) {
+        cameraOrLibraryAlert()
     }
     
     @IBAction func reviewButtonPressed(_ sender: UIButton) {
@@ -281,6 +301,20 @@ extension SpotDetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Review", for: indexPath) as! SpotReviewsTableViewCell
         cell.review = reviews.reviewArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension SpotDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.photoArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! SpotPhotosCollectionViewCell
+        cell.photo = photos.photoArray[indexPath.row]
         return cell
     }
     
